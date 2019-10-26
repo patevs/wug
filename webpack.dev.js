99cee07c17d7b82d5f255d8647329b1910d4088f
PatEvs <ppevans11@gmail.com>
2019-10-26 20:01:09 +1300

    ✨  webpack.dev.js

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+/**
+ * `webpack.dev.js`
+ */
+
+// IMPORTS
+
+// const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+// EXPORTS
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+});
+
+/* EOF */
