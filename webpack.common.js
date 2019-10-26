228b3c5d43ef3a9a57e668d93a961fe05ed82d9c
PatEvs <ppevans11@gmail.com>
2019-10-26 20:00:10 +1300

    ✨  webpack.common.js

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+
+/**
+ * `webpack.common.js`
+ */
+
+// IMPORTS
+
+const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// EXPORTS
+
+module.exports = {
+    entry: {
+        app: './index.js',
+    },
+    plugins: [],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [],
+    },
+};
+
+/* EOF */
