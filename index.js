00728f1f709b09f7b67d243ac84b2dd52bb9c8d4
PatEvs <ppevans11@gmail.com>
2019-10-27 19:16:53 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -8,8 +8,9 @@
  * * IMPORTS *
  *************/
 
+const spawn = require('cross-spawn');
+ // const Table = require('cli-table3');
 const termSize = require('term-size');
-const Table = require('cli-table3');
 
 /***************
  * * FUNCTIONS *
@@ -17,6 +18,15 @@ const Table = require('cli-table3');
 
 const _termsize = termSize();
 console.log(_termsize);
+
+const run = () => {
+	// Spawn NPM asynchronously
+	// const child = spawn('npm', ['list', '-g', '-depth', '0'], { stdio: 'inherit' });
+	// Spawn NPM synchronously
+	const result = spawn.sync('npm', ['list', '-g', '-depth', '0'], { stdio: 'inherit' });
+	console.log(result);
+}
+run();
 
 // instantiate
 // var table = new Table({
