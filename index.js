86368f2c4aa79204db836311791b0409a462a135
PatEvs <ppevans11@gmail.com>
2019-10-27 19:36:37 +1300

    📦  Refactoring

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
  * * IMPORTS *
  *************/
 
-const spawn = require('cross-spawn');
+const execa = require('execa');
+// const spawn = require('cross-spawn');
 const Table = require('cli-table3');
 const termSize = require('term-size');
 
@@ -16,31 +17,54 @@ const termSize = require('term-size');
  * * FUNCTIONS *
  ***************/
 
-// Get the size of the current terminal window
-const _termsize = termSize();
-// console.log(_termsize);
-const columns = _termsize.columns;
-// console.log(columns);
-const col = Math.floor(columns / 4);
-// console.log(col);
+/**
+ *	Gets and returns the size of the current terminal window
+ */
+const getTermSize = () => {
+	const _termsize = termSize();
+	// console.log(_termsize);
+	const columns = _termsize.columns;
+	// console.log(columns);
+	const col = Math.floor(columns / 4);
+	// console.log(col);
+	return col;
+}
+
+/**
+ * Creates and displays a table
+ */
+const genTable = () => {
+	// Instantiate a new table
+	let table = new Table({
+		head: ['COL 1', 'COL 2', 'COL 3'],
+		colWidths: [col, col, col]
+	});
+	// table is an Array, so you can `push`, `unshift`, `splice` and friends
+	table.push(
+		['one', 'two', 'three']
+	);
+	console.log(table.toString());
+}
 
 // Get NodeJS version
-const result = spawn.sync('node', ['--version'], { stdio: 'inherit' });
+// const result = spawn.sync('node', ['--version'], { stdio: 'inherit' });
+// console.log(result.toString());
 
-console.log(result.toString());
+/*
+(async () => {
+	const {stdout} = await execa('echo', ['unicorns']);
+	console.log(stdout);
+})();
+*/
 
-// Instantiate a new table
-let table = new Table({
-	head: ['COL 1', 'COL 2', 'COL 3'],
-	colWidths: [col, col, col]
-});
+/*****************
+ * * ENTRY POINT *
+ *****************/
 
-// table is an Array, so you can `push`, `unshift`, `splice` and friends
-table.push(
-	['one', 'two', 'three']
-);
-
-console.log(table.toString());
+(() => {
+	getTermSize();
+	genTable();
+})();
 
 /*************
  * * EXPORTS *
