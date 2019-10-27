6e8149ff1c34ba3711fd8bd411ee036e9e820c15
PatEvs <ppevans11@gmail.com>
2019-10-27 19:08:03 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ const _termsize = termSize();
 console.log(_termsize);
 
 // instantiate
-var table = new Table({
-	head: ['TH 1 label', 'TH 2 label'],
+// var table = new Table({
+// 	head: ['TH 1 label', 'TH 2 label'],
 	// colWidths: [100, 200]
-});
+// });
 
 // table is an Array, so you can `push`, `unshift`, `splice` and friends
-table.push(
-	['First value', 'Second value'],
-	['First value', 'Second value']
-);
+// table.push(
+// 	['First value', 'Second value'],
+// 	['First value', 'Second value']
+// );
 
-console.log(table.toString());
+// console.log(table.toString());
 
 /*************
  * * EXPORTS *
