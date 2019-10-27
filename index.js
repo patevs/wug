f2f100d629919d3d8e19e877a204ade7d8bf3cb0
PatEvs <ppevans11@gmail.com>
2019-10-27 18:00:00 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -5,35 +5,22 @@
 
 'use strict';
 
-// IMPORTS
+/*************
+ * * IMPORTS *
+ *************/
 
-var Table = require('cli-table3');
-const execa = require('execa');
 const termSize = require('term-size');
 
-(async () => {
-	const {stdout} = await execa('echo', ['unicorns']);
-	console.log(stdout);
-})();
+/***************
+ * * FUNCTIONS *
+ ***************/
 
 termSize();
 
-// instantiate
-var table = new Table({
-	head: ['TH 1 label', 'TH 2 label'],
-	// colWidths: [100, 200]
-});
+/*************
+ * * EXPORTS *
+ *************/
 
-// table is an Array, so you can `push`, `unshift`, `splice` and friends
-table.push(
-	['First value', 'Second value'],
-	['First value', 'Second value']
-);
-
-console.log(table.toString());
-
-// EXPORTS
-
-module.exports = {};
+// module.exports = {};
 
 /* EOF */
