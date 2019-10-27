d80fcb3d6428b27f7bdc7b0e23d32cd55b774e52
PatEvs <ppevans11@gmail.com>
2019-10-27 18:18:41 +1300

    ✨  examples/

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
  *************/
 
 const termSize = require('term-size');
-const platform = require('platform');
+
 
 /***************
  * * FUNCTIONS *
@@ -18,18 +18,6 @@ const platform = require('platform');
 
 const _termsize = termSize();
 console.log(_termsize);
-
-const os = platform.os;
-// console.log(os);
-
-// const arch = os.architecture;
-// console.log(arch);
-// const fam = os.family;
-// console.log(fam);
-const oss = os.toString();
-console.log(oss);
-// const ver = os.version;
-// console.log(ver);
 
 /*************
  * * EXPORTS *
