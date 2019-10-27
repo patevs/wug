1d162c4bcf5f165ecfbaac2cbe03ab387624c412
PatEvs <ppevans11@gmail.com>
2019-10-27 18:19:55 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
  *************/
 
 const termSize = require('term-size');
+const Table = require('cli-table3');
 
 /***************
  * * FUNCTIONS *
@@ -16,6 +17,20 @@ const termSize = require('term-size');
 
 const _termsize = termSize();
 console.log(_termsize);
+
+// instantiate
+var table = new Table({
+	head: ['TH 1 label', 'TH 2 label'],
+	// colWidths: [100, 200]
+});
+
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
+table.push(
+	['First value', 'Second value'],
+	['First value', 'Second value']
+);
+
+console.log(table.toString());
 
 /*************
  * * EXPORTS *
