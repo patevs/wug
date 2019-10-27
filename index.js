2882b5ff18d25f84d5a1109cc3432e1ecbc6a7e4
PatEvs <ppevans11@gmail.com>
2019-10-27 21:00:34 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -24,10 +24,16 @@ const detect = require('./src/detect');
 	console.log("\nStarting WUG...\n");
 	// const col = getTermSize();
 	// genTable(col);
-	const nodev = await detect.getVersion('node')
+	const nodev = await detect.getDashDashVersion('node')
 	console.log(nodev);
-	const npmv = await detect.getVersion('npm')
+	const npmv = await detect.getDashDashVersion('npm')
 	console.log(npmv);
+	const gitv = await detect.getDashDashVersion('git')
+	console.log(gitv);
+	const hubv = await detect.getVersion('hub')
+	console.log(hubv);
+	const javav = await detect.getDashVersion('java')
+	console.log(javav);
 })();
 
 /*************
