03d34073109edc838b6a70eb3da33790d8f50927
PatEvs <ppevans11@gmail.com>
2019-10-27 18:16:57 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -10,14 +10,26 @@
  *************/
 
 const termSize = require('term-size');
+const platform = require('platform');
 
 /***************
  * * FUNCTIONS *
  ***************/
 
 const _termsize = termSize();
-
 console.log(_termsize);
+
+const os = platform.os;
+// console.log(os);
+
+// const arch = os.architecture;
+// console.log(arch);
+// const fam = os.family;
+// console.log(fam);
+const oss = os.toString();
+console.log(oss);
+// const ver = os.version;
+// console.log(ver);
 
 /*************
  * * EXPORTS *
