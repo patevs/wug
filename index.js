8b4ab775ea5ee8ae092d3864b4638f1f8f371161
PatEvs <ppevans11@gmail.com>
2019-10-27 20:30:11 +1300

    ✨  index.js

--- index.js
+++ index.js
@@ -8,63 +8,26 @@
  * * IMPORTS *
  *************/
 
-const execa = require('execa');
-const Table = require('cli-table3');
-const termSize = require('term-size');
+//
 
 /***************
  * * FUNCTIONS *
  ***************/
 
-/**
- *	Gets and returns the size of the current terminal window
- */
-const getTermSize = () => {
-	const _termsize = termSize();
-	// console.log(_termsize);
-	const columns = _termsize.columns;
-	// console.log(columns);
-	const col = Math.floor(columns / 4);
-	// console.log(col);
-	return col;
-}
-
-/**
- * Creates and displays a table
- */
-const genTable = (col) => {
-	// Instantiate a new table
-	let table = new Table({
-		head: ['COL 1', 'COL 2', 'COL 3'],
-		colWidths: [col, col, col]
-	});
-	// table is an Array, so you can `push`, `unshift`, `splice` and friends
-	table.push(
-		['one', 'two', 'three']
-	);
-	console.log(table.toString());
-}
-
-/**
- *	Gets the version of a given command
- */
-const getVersion = async (command) => {
-	const {stdout} = await execa(command, ['--version']);
-	// console.log(stdout);
-	return stdout;
-}
+//
 
 /*****************
  * * ENTRY POINT *
  *****************/
 
 (async () => {
+	console.log("Starting WUG...");
 	// const col = getTermSize();
 	// genTable(col);
-	const node = await getVersion('node');
-	console.log(node);
-	const npm = await getVersion('npm');
-	console.log(npm);
+	// const node = await getVersion('node');
+	// console.log(node);
+	// const npm = await getVersion('npm');
+	// console.log(npm);
 })();
 
 /*************
