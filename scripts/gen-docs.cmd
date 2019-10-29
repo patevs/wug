0850110c4782c9bfb4d9bd97d7c80d578607b8cd
PatEvs <ppevans11@gmail.com>
2019-10-29 19:34:47 +1300

    ✨  Updates to scripts/gen-docs.cmd

--- scripts/gen-docs.cmd
+++ scripts/gen-docs.cmd
@@ -21,11 +21,11 @@ WHERE curl
 IF %ERRORLEVEL% NEQ 0 ECHO. & ECHO [91m Curl installation could not be found... exiting! [0m & GOTO :EOF
 
 :: Remove old docs directory if exists
-:: IF EXIST ..\docs (
-::     ECHO. && ECHO [96m Deleting old [4mdocs[0m [96mdirectory... [0m
-::     RMDIR /S /Q ..\docs
-::     ECHO [92m  Done! [0m
-:: )
+IF EXIST ..\docs (
+    ECHO. && ECHO [96m Deleting old [4mdocs[0m [96mdirectory... [0m
+    RMDIR /S /Q ..\docs
+    ECHO [92m  Done! [0m
+)
 
 :: Create docs\dependencies directory
 ECHO. && ECHO [96m Creating [4mdocs\dependencies[0m [96mdirectory... [0m
