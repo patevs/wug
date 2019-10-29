921b94311d70ecbe6cc7994a8d5f6827f2b539c3
PatEvs <ppevans11@gmail.com>
2019-10-29 20:11:20 +1300

    ✨  Created docs/examples/

---
+++ docs/examples/cli-table3.js
@@ -0,0 +1,42 @@
+/****************************
+ * * examples/cli-table3.js *
+ ****************************/
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// var Table = require('cli-table3');
+const Table = require('cli-table3');
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+// By default, headers will be red, and borders will be grey
+// var table = new Table({head:['a','b']});
+// table.push(['c','d']);
+
+// instantiate
+var table = new Table({
+	head: ['TH 1 label', 'TH 2 label'],
+	// colWidths: [100, 200]
+});
+
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
+table.push(
+	['First value', 'Second value'],
+	['First value', 'Second value']
+);
+
+console.log(table.toString());
+
+/*************
+ * * EXPORTS *
+ *************/
+
+// module.exports = {};
+
+/* EOF */
---
+++ docs/examples/cross-spawn.js
@@ -0,0 +1,33 @@
+/*****************************
+ * * examples/cross-spawn.js *
+ *****************************/
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+const spawn = require('cross-spawn');
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+const run = () => {
+	// Spawn NPM asynchronously
+	// const child = spawn('npm', ['list', '-g', '-depth', '0'], { stdio: 'inherit' });
+	// Spawn NPM synchronously
+	// const result =
+	spawn.sync('npm', ['list', '-g', '-depth', '0'], { stdio: 'inherit' });
+	// console.log(result);
+}
+run();
+
+/*************
+ * * EXPORTS *
+ *************/
+
+// module.exports = {};
+
+/* EOF */
