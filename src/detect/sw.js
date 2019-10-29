9b4ec7dff9356f927eb46ead2c7fbbda5589c109
PatEvs <ppevans11@gmail.com>
2019-10-29 21:25:54 +1300

    ✨  Created src/detect/

---
+++ src/detect/sw.js
@@ -0,0 +1,41 @@
+/**********************
+ * * src/detect/sw.js *
+ **********************/
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// const execa = require('execa');
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/*
+const version = async command => {
+    const { stdout } = await execa(command, ['version']);
+    return stdout;
+};
+
+const _version = async command => {
+    const { stdout } = await execa(command, ['-version']);
+    return stdout;
+};
+
+const __version = async command => {
+    const { stdout } = await execa(command, ['--version']);
+    // console.log(stdout);
+    return stdout;
+};
+*/
+
+/*************
+ * * EXPORTS *
+ *************/
+
+// module.exports = { };
+
+/* EOF */
