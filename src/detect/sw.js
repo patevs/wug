b57f4a126270bae8df63ef625c3ed70478986126
PatEvs <ppevans11@gmail.com>
2019-10-29 21:40:19 +1300

    ✨  detect/sw.js

--- src/detect/sw.js
+++ src/detect/sw.js
@@ -14,26 +14,28 @@ const execa = require('execa');
  * * FUNCTIONS *
  ***************/
 
+/*
 const dynamicDetect = async (command, args) => {
     //..
     const { stdout } = await execa(command, args);
     return stdout;
     //..
 }
+*/
 
 //
-const detect = async (name) => {
+const detect = name => {
     //..
     // Run switch on name
     switch (name) {
         case 'node':
-            // console.log('NodeJS');
-            return await dynamicDetect('node', '-v');
-            // break;
+            console.log('NodeJS');
+            // return await dynamicDetect('node', '-v');
+            break;
         case 'npm':
-            // console.log('npm');
-            return await dynamicDetect('npm', '-v');
-            // break;
+            console.log('npm');
+            // return await dynamicDetect('npm', '-v');
+            break;
         default:
             console.log('%s command not understood', name);
     };
